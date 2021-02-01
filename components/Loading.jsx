7a2755de779472581dd45574739b797efc00651e
Kuba Budzyński <main.kubabudzynski@gmail.com>
2021-02-01 01:27:38 +0100

    switch to Font Awesome spinner icon

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import {CgSpinner} from "react-icons/Cg";
+import {FaSpinner} from "react-icons/fa";
 
 const Loading = () => (
   <div className="mx-auto p-2 align-center flex align-center justify-center flex-0">
-    <CgSpinner size="2em" className="animate-spin" />
+    <FaSpinner size="2em" className="animate-spin" />
   </div>
 )
 
