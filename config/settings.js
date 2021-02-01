de8b3e10922fb4f538823612fe15be751486d859
Kuba Budzyński <main.kubabudzynski@gmail.com>
2021-02-01 01:03:16 +0100

    Setting up for vercel hosting

--- config/settings.js
+++ config/settings.js
@@ -1,7 +1,7 @@
 // Provide system variables for an app in a single object
 
 const SETTINGS = {
-    apiRoot: process.env.API_ROOT || "http://localhost:7000",
+    apiRoot: "https://app-back.tk:7000",
     token: () => typeof window !== "undefined" && localStorage.getItem("teamId") || null,
 }
 
