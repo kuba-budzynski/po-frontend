f1dd8024c6557f06c488b1c8d74b6c9da46d4c73
Karol Sitarz <ka.sitarz@gmail.com>
2021-02-04 15:35:27 +0100

    separate env variables
    commit 5e811550842563ef76e177ff9ca35f4d627549aa
    Author: Karol Sitarz <ka.sitarz@gmail.com>
    Date:   Thu Feb 4 15:34:56 2021 +0100
    
        add public api prefix
    
    commit 406472df888606ef92eed869ed0b380e632965a5
    Author: Karol Sitarz <ka.sitarz@gmail.com>
    Date:   Thu Feb 4 15:11:20 2021 +0100
    
        update missed api root in settings
    
    commit d4e78a14d85a8f2b0778d088cfd0494b0a69219c
    Author: Karol Sitarz <ka.sitarz@gmail.com>
    Date:   Thu Feb 4 13:27:14 2021 +0100
    
        separate env variables

--- config/settings.js
+++ config/settings.js
@@ -1,7 +1,7 @@
 // Provide system variables for an app in a single object
 
 const SETTINGS = {
-    apiRoot: "https://app-back.tk:7000",
+    apiRoot: process.env.NEXT_PUBLIC_API_ROOT,
     token: () => typeof window !== "undefined" && localStorage.getItem("teamId") || "5ffdea931b0b224a68fd6a10",
 }
 export default SETTINGS;
